18fa196af1d1e0e6210e1741b7f48dcdca876f5e
Christopher Handy <handyc@helxt.com>
2025-06-25 22:26:36 +0200

    Update tbd.sh
    more prettier

--- bash/tbd.sh
+++ bash/tbd.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 journal=$HOME/tbdjournal
 mkdir -p $journal
-now=$(date +"%Y_%b_%d_%H_%M_%S"); read -p "Entry: " entry; echo $entry > $journal/$now
+now=$(date +"%Y_%b_%d_%H_%M_%S") 
+read -p "Entry: " entry
+echo $entry > $journal/$now
